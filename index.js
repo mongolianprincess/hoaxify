e965c23f30aaf94ee673c02af006b9be888e92ed
Amy <ayc.yang@gmail.com>
2022-01-12 18:44:39 -0800

    add node env and config for database in different env

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const app = require('./src/app');
 const sequelize = require('./src/config/database');
 
 sequelize.sync();
+console.log('env: ' + process.env.NODE_ENV);
 
 app.listen(3000, () => console.log('app is running!!!!!'));
